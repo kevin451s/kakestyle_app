9799e1002a8d53c2c99f79970056139543221d26
Kevin Salazar <kskevin170@yahoo.com>
2016-10-25 10:48:07 -0700

    adds seeds and pry

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+garments = Garment.create([
+  {
+    name: 'hoodie', color: 'grey', material: 'cotton', price:50, cool: true
+  },
+  {
+    name: 'jeans', color: 'blue', material: 'denim', price: 30, cool: true
+  },
+  {
+    name: 'dress', color: 'black', material: 'silk', price: 120, cool: false
+  }
+  ])
